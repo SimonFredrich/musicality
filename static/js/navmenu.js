21edcac7eb7565815803be342bee3c9f2ce827f7
Vagus-art <agustin.favo@gmail.com>
2019-12-20 00:16:10 -0300

    this fixes #2 I had to move all sidemenu's class methods to it's constructor, for some reason the previous version worked on PC,

--- static/js/navmenu.js
+++ static/js/navmenu.js
@@ -2,21 +2,21 @@ export default class navmenu {
     constructor(Overlay,Menu){
         this.Overlay=Overlay;
         this.Menu=Menu;
-    }
-    unwrapSideMenu = () => {
-        this.Overlay.style.animationName = "sideMenOverAnimation";
-        this.Overlay.style.animationDuration = "0.3s"
-        this.Overlay.style.width= "100%";
-        this.Menu.style.animationName = "sideMenAnimation";
-        this.Menu.style.animationDuration = "0.2s"
-        this.Menu.style.width= "300px";
-    }
-    wrapSideMenu = () => {     
-        this.Overlay.style.animationName = "sideMenOverAnimationWrap";
-        this.Overlay.style.animationDuration = "0.2s"
-        this.Overlay.style.width= "0%";
-        this.Menu.style.animationName = "sideMenAnimationWrap";
-        this.Menu.style.animationDuration = "0.2s"
-        this.Menu.style.width= "0%";
-    }
+        this.unwrapSideMenu = () => {
+            this.Overlay.style.animationName = "sideMenOverAnimation";
+            this.Overlay.style.animationDuration = "0.3s"
+            this.Overlay.style.width= "100%";
+            this.Menu.style.animationName = "sideMenAnimation";
+            this.Menu.style.animationDuration = "0.2s"
+            this.Menu.style.width= "300px";
+        }
+        this.wrapSideMenu = () => {     
+            this.Overlay.style.animationName = "sideMenOverAnimationWrap";
+            this.Overlay.style.animationDuration = "0.2s"
+            this.Overlay.style.width= "0%";
+            this.Menu.style.animationName = "sideMenAnimationWrap";
+            this.Menu.style.animationDuration = "0.2s"
+            this.Menu.style.width= "0%";
+        }
+    }  
 }
