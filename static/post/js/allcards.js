ab9d5073e5d2026d6d33d65d1bd3bd825003992b
Vagus-art <agustin.favo@gmail.com>
2019-12-30 23:25:32 -0300

    deleted useless folder

--- static/post/js/allcards.js
+++
@@ -1,8 +0,0 @@
-import fetchcards from './fetchcards.js';
-import cardformatter from "./cardformatter.js";
-//card fetcher
-export default ()=>{
-    const cardcontainer = document.getElementById('coursescontent');
-    const cardfetcher = fetchcards('/api');
-    cardfetcher.then(res=>cardformatter(res,cardcontainer));
-}
--- static/post/js/fetchcards.js
+++
@@ -1,9 +0,0 @@
-export default async function fetchcards(route) { //why?
-        try {
-          const resp = await fetch(route)
-          const newresp = await resp.json();
-          return newresp;
-        } catch (err) {
-             console.log(err)
-        }
-    }    
