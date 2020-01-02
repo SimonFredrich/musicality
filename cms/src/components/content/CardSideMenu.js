821072283b058a708267255fc1a437ecf9f6d761
Vagus-art <agustin.favo@gmail.com>
2020-01-02 12:34:45 -0300

    added icons from flatico

--- cms/src/components/content/CardSideMenu.js
+++ cms/src/components/content/CardSideMenu.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './cards.css';
+import {ReactComponent as DeleteIcon} from './svg/rubbish-bin.svg';
+import {ReactComponent as EditIcon} from './svg/pencil-edit.svg';
 
 export default class CardSideMenu extends Component {
     render() {
         return (
             <div id={this.props.id} className="card-sidemenu">
-            <button onClick={()=>(this.props.deletebyid(this.props.id))}>delete</button>
+            <button className="sidemenu-btn" onClick={()=>(this.props.deletebyid(this.props.id))}><DeleteIcon className="sidemenu-icon" /></button>
+            <button className="sidemenu-btn" onClick={()=>(this.props.deletebyid(this.props.id))}><EditIcon className="sidemenu-icon edit" /></button>
             </div>
         )
     }
