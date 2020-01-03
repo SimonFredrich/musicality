334bbee01408e680a5942f470ce01e5013bdfecc
Vagus-art <agustin.favo@gmail.com>
2020-01-03 20:17:55 -0300

    +

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 const database = require('./database.js');
 const router = require('./routes.js');
 const postapi = require('./postapi.js');
