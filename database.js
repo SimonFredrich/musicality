0ebe9a8095fc5937632b29bb12804f81edd7c736
Vagus-art <agustin.favo@gmail.com>
2020-01-03 21:04:57 -0300

    now connects to atlas?
    Signed-off-by: Vagus-art <agustin.favo@gmail.com>

--- database.js
+++ database.js
@@ -1,3 +1,8 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/musicality', {useNewUrlParser: true},()=>console.log('database is connected'));
-module.exports = mongoose;
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:<password>@musicality-v04uq.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
