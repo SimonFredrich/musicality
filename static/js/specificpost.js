720e6263fd29bb7f669d51b22c9c8ceb1910209c
Vagus-art <agustin.favo@gmail.com>
2020-01-05 03:51:42 -0300

    commented for easy switch between production and development (CMS)

--- static/js/specificpost.js
+++ static/js/specificpost.js
@@ -3,6 +3,6 @@ import postformatter from "./postformatter.js";
 //card fetcher
 export default ()=>{
     const postcontainer = document.getElementById('courses');
-    const cardfetcher = fetchcards('/api/' + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) );
+    const cardfetcher = fetchcards('/api/' + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) ); //gets post content from current url
     cardfetcher.then(res => postformatter(res,postcontainer));
 }
